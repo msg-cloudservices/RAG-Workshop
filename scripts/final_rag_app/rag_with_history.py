c1a3e432ceb5d271e42d9af4b8ca2b55ddafedaf
Jonas Lossin <jonas.lossin@msg.group>
2024-02-22 11:49:36 +0100

    markdown printing of messages in streamlit app

--- scripts/final_rag_app/rag_with_history.py
+++ scripts/final_rag_app/rag_with_history.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
 from langchain.chains import LLMChain, ConversationChain
 from langchain.prompts import PromptTemplate
-from langchain.memory import  ChatMessageHistory, ConversationBufferWindowMemory
+from langchain.memory import ChatMessageHistory, ConversationBufferWindowMemory
 import streamlit as st
 import openai
+
 # from config_openai import *
 from openai import AzureOpenAI
 
@@ -13,11 +14,11 @@ from azure_ai_search import get_doc_azure_ai
 
 load_dotenv()
 
-#importing Azure OpenAI creds
+# importing Azure OpenAI creds
 api_key = os.getenv("AZURE_OPENAI_KEY")
-os.environ["OPENAI_API_KEY"]= api_key
+os.environ["OPENAI_API_KEY"] = api_key
 azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-deployment_name='gpt' 
+deployment_name = "gpt"
 
 # Load environment variables
 # AZURE_OPENAI_KEY = os.getenv("AZURE_OPENAI_KEY")
@@ -26,31 +27,31 @@ deployment_name='gpt'
 # Initialize the LLM (Language Model) with Azure OpenAI credentials
 llm = AzureChatOpenAI(
     api_version="2023-12-01-preview",
-    api_key=api_key,  
-    azure_endpoint = azure_endpoint,
-    deployment_name="gpt", 
+    api_key=api_key,
+    azure_endpoint=azure_endpoint,
+    deployment_name="gpt",
     model_name="gpt-35-turbo",
     temperature=0.9,
-    )
+)
 
 client = AzureOpenAI(
-  azure_endpoint = azure_endpoint, 
-  api_key=api_key,  
-  api_version="2023-05-15"
+    azure_endpoint=azure_endpoint, api_key=api_key, api_version="2023-05-15"
 )
+
 
 def generate_answer(conversation):
     response = client.chat.completions.create(
-    model="gpt",
-    messages=conversation,
-    temperature=0,
-    max_tokens=1000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop = [' END']
+        model="gpt",
+        messages=conversation,
+        temperature=0.7,
+        max_tokens=3000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
     )
     return (response.choices[0].message.content).strip()
+
 
 # conversation = []
 
@@ -58,17 +59,24 @@ def generate_answer(conversation):
 st.title("Let's chat")
 
 # Session state to store chat history
-if 'history' not in st.session_state:
-    st.session_state['history'] = [{"role": "system", "content": "Am Anfang jeder Nachricht steht ein Kontext, den du verwendest, um deine Antworten zu erstellen. Du beantwortest hauptsächlich Fragen zu Versicherungsthemen."}]
+if "history" not in st.session_state:
+    st.session_state["history"] = [
+        {
+            "role": "system",
+            "content": "Am Anfang jeder Nachricht steht ein Kontext, den du verwendest, um deine Antworten zu erstellen. Dieser Kontext ist ein Auszug aus Versicherungstexten der DEVK. Du beantwortest hauptsächlich Fragen zu Versicherungsthemen mit Hilfe dieser Kontexte.",
+        }
+    ]
 
 user_input = st.chat_input("You:", key="input")
 
 if user_input:
     # get matching text from Azure AI Search and create prompt
     context = "\n".join(get_doc_azure_ai(user_input))
-    st.session_state.history.append({"role": "user", "content": f"Context: {context}\n\n{user_input}"})
+    st.session_state.history.append(
+        {"role": "user", "content": f"Context: {context}\n\n{user_input}"}
+    )
     rag_messages = f"context: {context}"
-    
+
     # generate response and append it
     response = generate_answer(st.session_state.history)
     st.session_state.history.append({"role": "assistant", "content": response})
@@ -76,7 +84,9 @@ if user_input:
 # Display chat history
 for message in st.session_state.history:
     if message["role"] == "user":
-        output = message['content'].split('\n\n', 1)
-        st.text(f"You: {output[1]}")
+        output = message["content"].split("\n\n", 1)
+        # Using st.write for better formatting with Markdown
+        st.write(f"**You:** {output[1]}")
     if message["role"] == "assistant":
-        st.text(f"ChatGPT: {message['content']}")
+        # Using st.write for better formatting with Markdown
+        st.write(f"**ChatGPT:** {message['content']}")
